504c44dfcc29941e9243c9ac4cb7226799ad9913
Marcos Lopez <marcos.lopez@viseo.com>
2020-08-26 15:01:03 +0200

    addedn async operations

--- src/ML.Data.Contracts/Respositories/IQueryRepository.cs
+++ src/ML.Data.Contracts/Respositories/IQueryRepository.cs
@@ -2,22 +2,26 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ML.Data.Contracts.Respositories
 {
     public interface IQueryRepository<TEntity> where TEntity : class
     {
-        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate, bool trackingEnabled = false);
 
-        TEntity FindById(object id);
+        TEntity FindById(object id);        
+        Task<TEntity> FindByIdAsync(object[] id, CancellationToken cancellationToken = default);
 
         TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate, bool trackingEnabled = false);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default, bool trackingEnabled = false)
 
         PagedList<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, int page = 0, int pageSize = 50, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, bool trackingEnabled = false);
         PagedList<TEntity> Get(IQueryable<TEntity> query, int page = 0, int pageSize = 50, bool trackingEnabled = false);
         IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, bool trackingEnabled = false);
 
-        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> GetAll(bool trackingEnabled = false);
 
     }
 }
--- src/ML.Infrastructure.Persistence.EF/Repositories/QueryRepository.cs
+++ src/ML.Infrastructure.Persistence.EF/Repositories/QueryRepository.cs
@@ -4,6 +4,8 @@ using ML.DataStructure.Collections;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ML.Infrastructure.Persistence.EF.Repositories
 {
@@ -11,31 +13,35 @@ namespace ML.Infrastructure.Persistence.EF.Repositories
     {
         protected DbContext Context;
         protected DbSet<TEntity> _DbSet;
-               
+
         public QueryRepository(DbContext context)
         {
             Context = context;
             _DbSet = context.Set<TEntity>();
         }
 
-        public virtual IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
-        {
-            return ((IQueryable<TEntity>)_DbSet).Where(predicate);
-        }
+        public virtual IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate, bool trackingEnabled = false)
+            => GetAll(trackingEnabled).Where(predicate);
 
         public TEntity FindById(object id)
-        {
-            return _DbSet.Find(id);
-        }
+            => _DbSet.Find(id);
+        public Task<TEntity> FindByIdAsync(object[] id, CancellationToken cancellationToken = default)
+            => _DbSet.FindAsync(id, cancellationToken: cancellationToken).AsTask();
+
 
         public virtual TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate, bool trackingEnabled = false)
         {
             return (trackingEnabled ? (IQueryable<TEntity>)_DbSet : (IQueryable<TEntity>)_DbSet.AsNoTracking()).FirstOrDefault(predicate);
         }
 
-        public virtual IQueryable<TEntity> GetAll()
+        public virtual Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default, bool trackingEnabled = false)
         {
-            return (IQueryable<TEntity>)_DbSet;
+            return (trackingEnabled ? (IQueryable<TEntity>)_DbSet : (IQueryable<TEntity>)_DbSet.AsNoTracking()).FirstOrDefaultAsync(predicate, cancellationToken);
+        }        
+
+        public virtual IQueryable<TEntity> GetAll(bool trackingEnabled = false)
+        {
+            return (IQueryable<TEntity>)(trackingEnabled ? (IQueryable<TEntity>)_DbSet : (IQueryable<TEntity>)_DbSet.AsNoTracking());
         }
 
         public virtual PagedList<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
@@ -53,18 +59,18 @@ namespace ML.Infrastructure.Persistence.EF.Repositories
         }
 
         public virtual PagedList<TEntity> Get(IQueryable<TEntity> query,
-            int page = 0, int pageSize = 50,            
+            int page = 0, int pageSize = 50,
             bool trackingEnabled = false)
         {
             return new PagedList<TEntity>(trackingEnabled ? query : query.AsNoTracking(), page, pageSize);
         }
 
         public virtual IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,            
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             bool trackingEnabled = false)
         {
             IQueryable<TEntity> query = (IQueryable<TEntity>)_DbSet;
-            
+
             if (filter != null)
                 query = query.Where(filter);
             if (orderBy != null)
